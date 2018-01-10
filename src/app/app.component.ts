bc201d639e7da88a0a2c14e53a176fc814a60ff5
Alexandra Polisevschi <alexandra.polisevschi@mindit.ro>
2018-01-10 13:18:53 +0200

    functional app with menu and 3 pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
-  isCollapsed:boolean=true;
+  isCollapsed: boolean=true;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from '../components/login/login.component';
+import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { ArticleListComponent } from './components/article-list/article-list.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent,
+    ArticleListComponent,
+    UserListComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     NgbModule.forRoot()
   ],
   providers: [],
